eb815a023e26020c308415ac37a31b2760327de8
arielLevita <levita.ariel@gmail.com>
2024-02-12 00:40:00 -0300

    DetailsView added + Routing updated.

--- src/components/CardGrid.vue
+++ src/components/CardGrid.vue
@@ -3,85 +3,91 @@ import { getProducts } from '../scripts/firebase.js'
 import { RouterLink } from 'vue-router'
 
 export default {
-  data() {
-    return {
-      products: []
+    data() {
+        return {
+            products: []
+        }
+    },
+    async created() {
+        try {
+            const querySnapshot = await getProducts()
+            const products = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            this.products = products
+        } catch (error) {
+            console.error(error)
+        }
     }
-  },
-  async created() {
-    try {
-      const querySnapshot = await getProducts()
-      const products = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data()
-      }))
-      this.products = products
-    } catch (error) {
-      console.error(error)
-    }
-  }
 }
 </script>
 
 <template>
-  <div class="d-flex flex-wrap align-items-center justify-content-around row-gap-3">
-    <div
-      v-if="products.length > 0"
-      v-for="product in products"
-      :key="product.id"
-      class="card d-flex flex-row shadow p-2"
-      :id="'card' + product.id"
-    >
-      <div class="card-image rounded rounded-lg p-1 w-50 h-100">
-        <img :src="product.imageFront" :alt="product.name" class="img-fluid rounded rounded-lg" />
-      </div>
-      <div class="px-2 w-50 d-flex flex-column justify-content-between">
-        <div class="card-body p-1">
-          <p class="card-title text-balance fw-medium m-0">{{ product.name }}</p>
+    <div class="d-flex flex-wrap align-items-center justify-content-around row-gap-3">
+        <div
+            v-if="products.length > 0"
+            v-for="product in products"
+            :key="product.id"
+            class="card d-flex flex-row shadow p-2"
+            :id="'card' + product.id"
+        >
+            <div class="card-image rounded rounded-lg p-1 w-50 h-100">
+                <img
+                    :src="product.imageFront"
+                    :alt="product.name"
+                    class="img-fluid rounded rounded-lg"
+                />
+            </div>
+            <div class="px-2 w-50 d-flex flex-column justify-content-between">
+                <div class="card-body p-1">
+                    <p class="card-title text-balance fw-medium m-0">{{ product.name }}</p>
+                </div>
+                <div class="card-bottom">
+                    <div class="p-0 mb-1">
+                        <RouterLink :to="'../details/' + product.id" class="btn btn-sm shadow w-100"
+                            >Detalles</RouterLink
+                        >
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="card-bottom">
-          <div class="p-0 mb-1">
-            <RouterLink :to="'../details/' + product.id" class="btn btn-sm shadow w-100"
-            >Detalles</RouterLink
-            >
-          </div>
-        </div>
-      </div>
+        <h3 v-else class="display-1 fw-bolder text-center">
+            No se han encontrado productos que coincidan con la búsqueda.
+        </h3>
     </div>
-    <h3 v-else class="display-1 fw-bolder text-center">No se han encontrado productos que coincidan con la búsqueda.</h3>
-  </div>
 </template>
 
 <style scope>
 .card {
-  width: 25rem;
-  /* height: 13rem; */
-  background-color: var(--color-pastel-4);
+    width: 25rem;
+    /* height: 13rem; */
+    background-color: var(--color-pastel-4);
 }
 
 .card-image > img {
-  max-height: 100%;
-  aspect-ratio: 1 / 1;
-  background-repeat: no-repeat;
-  background-size: cover;
-  box-shadow:
-    rgba(0, 0, 0, 0.19) 0px 10px 20px,
-    rgba(0, 0, 0, 0.23) 0px 6px 6px;
+    max-height: 100%;
+    aspect-ratio: 1 / 1;
+    background-repeat: no-repeat;
+    background-size: cover;
+    box-shadow:
+        rgba(0, 0, 0, 0.19) 0px 10px 20px,
+        rgba(0, 0, 0, 0.23) 0px 6px 6px;
 }
 
 .btn {
-  background-color: var(--color-pastel-2);
-  color: #212529;
+    background-color: var(--color-pastel-2);
+    color: #212529;
 }
 
 .btn:hover {
-  background-color: var(--color-logo) !important;
-  color: var(--color-pastel-4) !important;
+    background-color: var(--color-logo) !important;
+    color: var(--color-pastel-4) !important;
 }
 
-@media only screen and (max-width:400px) {
-  .card-title {
-    font-size: smaller;
-  }
+@media only screen and (max-width: 400px) {
+    .card-title {
+        font-size: smaller;
+    }
 }
 </style>
---
+++ src/views/DetailsView.vue
@@ -0,0 +1,121 @@
+<script>
+import { getProducts } from '@/scripts/firebase'
+export default {
+    data() {
+        return {
+            product: null
+        }
+    },
+    async created() {
+        try {
+            const detailId = this.$route.params.id
+
+            const querySnapshot = await getProducts()
+            const products = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+
+            this.product = products.find((product) => product.id == detailId)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    methods: {
+        goBack() {
+            this.$router.go(-1)
+        }
+    }
+}
+</script>
+
+<template>
+    <div v-if="product" class="details-card row p-5 grid gap-3">
+        <div class="col-lg-8 p-0 details-image-container border border-dark">
+            <img :src="product.imageBack" alt="Product Image" />
+            <span>Pase el cursor para agrandar</span>
+        </div>
+        <div class="col p-3 details-data bg-light">
+            <div>
+                <h3 class="text-center p-3">{{ product.name }}</h3>
+                <ul>
+                    <li class="pt-2"><span>Marca:</span> {{ product.brand }}</li>
+                    <li class="pt-2"><span>Producto:</span> {{ product.name }}</li>
+                    <li class="pt-2"><span>Categoría:</span> {{ product.category }}</li>
+                    <li class="pt-2">
+                        <span>Tamaño de la porción:</span> {{ product.servingSize }}
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex justify-content-center">
+                <button @click="goBack" class="btn btn-outline-dark w-100 shadow m-2">
+                    Volver
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scope>
+.details-card {
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    background-color: var(--color-pastel-3);
+}
+
+.details-image-container {
+    box-shadow: -4px -4px 6px #888888;
+    aspect-ratio: 1 / 1;
+    overflow: hidden;
+    position: relative;
+    box-sizing: inherit;
+}
+
+.details-image-container:hover {
+    cursor: zoom-in;
+}
+
+.details-image-container img {
+    width: 100%;
+    position: absolute;
+    display: block;
+}
+
+.details-image-container span {
+    z-index: 5;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    -o-transform: translate(-50%, -50%);
+    background: rgba(0, 0, 0, 0.8);
+    display: block;
+    padding: 0.5rem 1rem;
+    font-family: sans-serif;
+    font-weight: bold;
+    color: var(--color-pastel-4);
+    text-align: center;
+}
+
+.details-image-container:hover span {
+    display: none;
+}
+
+.details-data {
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.details-data ul {
+    list-style-type: none;
+    list-style-position: inside;
+}
+
+.details-data li > span {
+    font-weight: bold;
+}
+</style>
